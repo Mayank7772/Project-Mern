29e998f09eb3ac7bf7a1f3776a978f3224c5306b
Mayank7772 <mayankrajput82258@gmail.com>
2025-07-11 12:18:49 +0530

    connect to data base

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('Error connecting to MongoDB:', err);
+});
 
 const app = express();
 
