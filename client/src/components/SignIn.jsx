3a43a1a98238c335b3b243e748b0ace51044c872
Mayank7772 <mayankrajput82258@gmail.com>
2025-07-13 15:46:46 +0530

    Add redux tool kit

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link , useNavigate} from "react-router-dom";
+import { useDispatch , useSelector} from "react-redux";
+import { singInStart  , singInFailure , singInSuccess} from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = React.useState({});
-  const [error, setError] = React.useState(null);
-  const [loading, setLoading] = React.useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
-  
+  const dispatch = useDispatch();
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -17,7 +19,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(singInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -27,17 +29,14 @@ export default function SignIn() {
       });
 
       const data = await res.json();
-      if (data.success == false) {
-        setLoading(false);
-        setError(data.message);
+      if (data.success === false) {
+        dispatch(singInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(singInSuccess(data));
       navigate('/')
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(singInFailure(error.message));
     }
   };
 
---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,31 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    currentUser : null ,
+    error : null ,
+    loading : false,
+}
+
+const userSlice = createSlice({
+    name : 'user',  
+    initialState,
+    reducers : {
+        singInStart : (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        singInSuccess : (state, action) => {
+            state.loading = false;
+            state.currentUser = action.payload;
+            state.error = null;
+        },
+        singInFailure : (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+
+    },
+});
+
+export const { singInStart, singInSuccess, singInFailure } = userSlice.actions;
+export default userSlice.reducer;
