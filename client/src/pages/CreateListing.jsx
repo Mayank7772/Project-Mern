9291bba9b4ca2e41b543317fa00bc1bbb44f79eb
Mayank7772 <mayankrajput82258@gmail.com>
2025-07-23 12:42:09 +0530

    create search api route

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -296,7 +296,7 @@ const CreateListing = () => {
               className="w-full px-4 py-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
               onChange={handleChange}
-              value={formData.bathroom}
+              value={formData.bathrooms}
             />
           </div>
         </div>
